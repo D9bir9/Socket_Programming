d63740091b1cb81919ace25de0cd2d72007b99d8
Dabira <taiwoadejumo1234@gmail.com>
2026-09-12 18:21:16 +0000

    Add a program that get address info of www.google.com

---
+++ addressinfo.c
@@ -0,0 +1,36 @@
+#include <netdb.h>
+#include <netinet/in.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <arpa/inet.h>
+
+int main(){
+    struct addrinfo hints;
+    struct addrinfo *servinfo;
+
+    memset(&hints, 0, sizeof(hints));
+    hints.ai_family = AF_INET;
+    hints.ai_socktype = SOCK_STREAM;
+
+    int status = getaddrinfo("www.google.com", "3490", &hints, &servinfo);
+    if (status != 0){
+        fprintf(stderr, "gai error: %s\n", gai_strerror(status));
+        exit(1);
+    }
+
+    struct addrinfo *info = servinfo;
+
+    while (info != NULL){
+        char ip[INET_ADDRSTRLEN];
+        inet_ntop(AF_INET, &info, ip, INET_ADDRSTRLEN);
+        printf ("protocol: %d\n",info->ai_protocol);
+        printf("Ip Address (IPv4): %s\n", ip);
+        info = info->ai_next;
+    }
+
+    freeaddrinfo(servinfo);
+    exit(1);
+}
