3e6bd167b5cb84a2516ed85c5148bbf06e8198db
Dabira <taiwoadejumo1234@gmail.com>
2026-09-15 01:06:59 +0000

    add error checking

--- accept.c
+++ accept.c
@@ -1,4 +1,5 @@
 #include <string.h>
+#include <stdio.h>
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <netdb.h>
@@ -11,28 +12,50 @@ int main(void){
     socklen_t addr_size;
     struct addrinfo hints, *res;
     int sockfd, new_fd; // listen on sock_fd, new connection on new_fd
+    int status;
 
     memset(&hints, 0, sizeof hints);
     hints.ai_family = AF_UNSPEC; // use IPv4 or IPv6, whichever
     hints.ai_socktype = SOCK_STREAM; // TCP stream sockets
     hints.ai_flags = AI_PASSIVE; // fill in my IP for me
 
-    getaddrinfo(NULL, MYPORT, &hints, &res);
+    status = getaddrinfo(NULL, MYPORT, &hints, &res);
+    if (status != 0) {
+        fprintf(stderr, "getaddrinfo error: %s\n", gai_strerror(status));
+        exit(1);
+    }
 
     sockfd = socket(res->ai_family, res->ai_socktype, res->ai_protocol);
+    if (sockfd == -1) {
+        perror("socket");
+        exit(1);
+    }
 
 
     // Reuse the socket address to avoid "Address already in use" error
     int yes = 1;
-    setsockopt(sockfd, SOL_SOCKET, SO_REUSEADDR, &yes, sizeof yes);
+    if (setsockopt(sockfd, SOL_SOCKET, SO_REUSEADDR, &yes, sizeof yes) == -1) {
+        perror("setsockopt");
+        exit(1);
+    }
 
-    bind(sockfd, res->ai_addr, res->ai_addrlen);
+    if (bind(sockfd, res->ai_addr, res->ai_addrlen) == -1) {
+        perror("bind");
+        exit(1);
+    }
 
-    listen(sockfd, BACKLOG);
+    if (listen(sockfd, BACKLOG) == -1) {
+        perror("listen");
+        exit(1);
+    }
 
     addr_size = sizeof their_addr;
 
     new_fd = accept(sockfd, (struct sockaddr *)&their_addr, &addr_size);
+    if (new_fd == -1) {
+        perror("accept");
+        exit(1);
+    }
 
     // ready to communicate on socket descriptor new_fd
 
